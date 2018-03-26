21266deb5eaa32784d75f1b3a849fd072dc40c94
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-26 08:13:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON series.id = characters.series_id
-  GROUP BY characters.species 
+  GROUP BY characters.species WHERE characters.species = 'human'
   ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
